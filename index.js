666a789fe0ae070e7552886501a54e1a03e8d66f
NocteaMoon <bv8p9t8f7t@privaterelay.appleid.com>
2026-07-04 13:45:19 +0000

    validation (commit)

--- index.js
+++ index.js
@@ -161,7 +161,7 @@ function onCmdCooldown(channel, user, cmd) {
   return false;
 }
 
-// ---------- ANTI-DOUBLON MULTI-CHAÎNES ----------
+// ---------- ANTI-DOUBLON MULTI-CHAÎNES (mécanisme d'origine, qui fonctionnait) ----------
 const crossChannelDedupe = new Map();
 const DEDUPE_WINDOW_MS = 4000;
 function isDuplicateAcrossChannels(user, cmdName) {
